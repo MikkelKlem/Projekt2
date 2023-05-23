6bd27590e60cd8d01f87b684e51c94bd48111095
Rune2292 <132446427+Rune2292@users.noreply.github.com>
2023-05-23 12:10:16 +0200

    Update centralUnit.h

--- MasterUnit/centralUnit.h
+++ MasterUnit/centralUnit.h
@@ -1,19 +1,36 @@
-#include "moduleSettings.h"
-#include "unitUARTdriver.h"
-
-
-class centralUnit
+#pragma once
+#include <string.h>
+class moduleSettings
 {
 public:
-    centralUnit(unitUARTdriver* UART);
-    void startConfig();
-    void endConfig();
-    void initIO();
-    
-    void setSavedArray(int number);
-private:
-    moduleSettings savedArray[5][4];
-	  moduleSettings unitConfig_[4];
-    unitUARTdriver* UART_;
+	moduleSettings(
+		bool con, 
+	char* name, 
+	char desiredRutine_,
+	char* activationTime_,
+	char* rutine1_,
+	char* rutine2_,
+	char* rutine3_,
+	char* rutine4_
+	);
+	moduleSettings();
+
+	moduleSettings(char adress);
+	
+	bool isConnected_;
+	char* unitName_;
+	char desiredRutine_;
+	char activationTime_[6];
+	int activationHour_;
+	int activationMinute_;
+	char* rutine1_;
+	char* rutine2_;
+	char* rutine3_;
+	char* rutine4_;
+
+
+
+	
+	char adress_;
 };
 
