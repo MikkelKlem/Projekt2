162e3a7e1b1a268a2af04e0474817bc44e66d732
Rune2292 <132446427+Rune2292@users.noreply.github.com>
2023-05-24 12:49:17 +0200

    Update moduleSettings.h

--- MasterUnit/moduleSettings.h
+++ MasterUnit/moduleSettings.h
@@ -15,11 +15,28 @@ public:
 	moduleSettings();
 
 	moduleSettings(char adress);
-	
+
+
+	bool getIsConnected() const;
+	void setIsConnected(bool connectionStatus);
+	char* getUnitName() const; 
+	char* getDesiredRutine();
+	void setDesiredRutine(char rutine);
+	const char* getActivationTime() const;
+	int getActivationHour() const;
+	void setActivationHour(int hour);
+	int getActivationMinute();
+	void setActivationMinute(int minute);
+	char* getRutine1() const;
+	char* getRutine2() const;
+	char* getRutine3() const;
+	char* getRutine4() const;
+	char activationTime_[6];
+
+private:	
 	bool isConnected_;
 	char* unitName_;
 	char desiredRutine_;
-	char activationTime_[6];
 	int activationHour_;
 	int activationMinute_;
 	char* rutine1_;
